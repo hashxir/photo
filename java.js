545f6df8c8c3ad8dc355d1d22a6adb094aa3175a
hashxir <108220991+hashxir@users.noreply.github.com>
2022-08-18 16:37:41 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,45 @@
+function ouvrirPage() {
+    var a = document.getElementById("searchBar").value;
+
+    if (a === "voiture") {
+        location.replace("car_pictures.html");
+    }
+
+    else if (a === "voiture") {
+        location.replace("car_pictures.html");
+    }
+    
+    else if (a === "voitures") {
+        location.replace("car_pictures.html");
+    }
+
+    else if (a === "Voiture") {
+        location.replace("car_pictures.html");
+    }
+
+    else if (a === "Voitures") {
+        location.replace("car_pictures.html");
+    }
+
+    else if (a === "macro") {
+        location.replace("macro.html");
+    }
+
+    else if (a === "Macro") {
+        location.replace("macro.html");
+    }
+
+    else if (a === "nature") {
+        location.replace("natural_pictures.html");
+    }
+
+    else if (a === "Nature") {
+        location.replace("natural_pictures.html");
+    }
+
+    else
+    {
+        location.replace("404.html")
+    }
+
+}
